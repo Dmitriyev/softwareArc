64338b9e28d79e77c80b46e3ef2d1f52403db261
Dmitriyev <evseev.dmsr@gmail.com>
2016-03-28 17:31:19 +0400

    Added files via upload

--- src/BusinessLogic/TestTask.java
+++ src/BusinessLogic/TestTask.java
@@ -7,12 +7,5 @@ import java.io.IOException;
  Тестовое задание
  */
 public class TestTask {
-    private String TaskFileName;
 
-    public TestTask(String TaskFileName) {
-        File file = new File(TaskFileName);
-        if (!file.exists()) {
-            // TODO не создавать объект класса
-        }
-    }
 }
